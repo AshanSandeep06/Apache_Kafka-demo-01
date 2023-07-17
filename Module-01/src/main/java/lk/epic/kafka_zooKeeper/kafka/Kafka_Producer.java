a786e541c8723ebb8be96018a9351f5656e891ba
AshanSandeep06 <ashansandeep06@gmail.com>
2023-07-17 22:35:08 +0530

    Created Kafka Producer and Configure producer

--- Module-01/src/main/java/lk/epic/kafka_zooKeeper/kafka/Kafka_Producer.java
+++ Module-01/src/main/java/lk/epic/kafka_zooKeeper/kafka/Kafka_Producer.java
@@ -1,5 +1,32 @@
 package lk.epic.kafka_zooKeeper.kafka;
 
-public class Kafka_Producer {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
 
+// To send messages to the Topic we are going to
+// use Spring Provided Kafka Template
+
+// We have created Kafka Producer which will use Kafka Template
+// to send messages to the topic
+@Service
+public class Kafka_Producer {
+    //Let's inject the Kafka template in this Spring Bean
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public Kafka_Producer(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    // Put a log statement in order to print this message
+    // Then can use, Spring-boot provided Default Logger
+    // To log the message
+    // Logger Instance to log this message
+    private static final Logger LOGGER = LoggerFactory.getLogger(Kafka_Producer.class);
+
+    public void sendMessages(String message) {
+        LOGGER.info(String.format("Message sent %s", message));
+        kafkaTemplate.send("topic-1", message);
+    }
 }
